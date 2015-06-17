27d20400ca57aa3466c7040d8e5593f5baffaa2b
yatno <yatno@outlook.com>
2015-06-17 16:24:38 +0700

    Update views_emails_NewMessage.php

--- messages/id/views_emails_NewMessage.php
+++ messages/id/views_emails_NewMessage.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  '<strong>New</strong> message' => '',
-  'Reply now' => '',
-  'sent you a new message:' => '',
+  '<strong>New</strong> message' => 'Pesan <strong>Baru</strong>',
+  'Reply now' => 'Balas sekarang',
+  'sent you a new message:' => 'Telah mengirim mu sebuah pesan',
 );
