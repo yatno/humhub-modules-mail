d9e299e84fde477bc905e3405367404fcaccd8cc
yatno <yatno@outlook.com>
2015-06-17 16:19:09 +0700

    Update forms_CreateMessageForm.php

--- messages/id/forms_CreateMessageForm.php
+++ messages/id/forms_CreateMessageForm.php
@@ -1,7 +1,7 @@
 <?php
 return array (
-  'Message' => '',
-  'Recipient' => '',
-  'Subject' => '',
-  'You could not send an email to yourself!' => '',
+  'Message' => 'Pesan',
+  'Recipient' => 'Penerima',
+  'Subject' => 'Judul',
+  'You could not send an email to yourself!' => 'Kamu tidak bisa mengirim email ke dirimu sendiri',
 );
